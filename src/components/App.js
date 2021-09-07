bff17ccdcfe211f14f6995d4896b91aaf271cc9b
Praveen Kumar <praveenscience@gmail.com>
2021-09-07 08:10:11 +0100

    Reorganised and removed useless files.

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Sadaf is banned!</h1>
+        <p>...only from Quizzes, because she answered it wrong!</p>
+      </div>
+    );
+  }
+}
+
+export default App;
