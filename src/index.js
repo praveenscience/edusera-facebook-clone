47b622e899b34c7828ca2cb6da78a9bfd2e69f5f
Praveen Kumar <praveenscience@gmail.com>
2021-09-08 07:12:38 +0100

    Added a basic SCSS and linked it.

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
 
+import "./styles/index.scss";
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
