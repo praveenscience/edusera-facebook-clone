f687e646876c2e6c08093681ea480c0759a7e91d
Praveen Kumar <praveenscience@gmail.com>
2021-09-09 07:44:31 +0100

    Created the Sidebar Links.

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,6 +1,44 @@
-import React from "react";
+import React, { Fragment } from "react";
 
 const Sidebar = () => {
+  const ContentList = {
+    Default: [
+      {
+        Link: "/",
+        Icon: "news",
+        Text: "News Feed"
+      },
+      {
+        Link: "/",
+        Icon: "chat",
+        Text: "Messenger"
+      },
+      {
+        Link: "/",
+        Icon: "watch",
+        Text: "Watch"
+      },
+      {
+        Link: "/",
+        Icon: "market",
+        Text: "Marketplace"
+      }
+    ],
+    Shortcuts: [
+      {
+        Link: "/",
+        Icon: "web",
+        Text: "Personal Website"
+      }
+    ],
+    Explore: [
+      {
+        Link: "/",
+        Icon: "yt",
+        Text: "YouTube Channel"
+      }
+    ]
+  };
   return (
     <div className="Sidebar col-2">
       <div className="UserMeta">
@@ -9,6 +47,24 @@ const Sidebar = () => {
           Zuck
         </a>
       </div>
+      {Object.keys(ContentList).map(section => (
+        <Fragment key={section}>
+          {section !== "Default" && <h3>{section}</h3>}
+          <ul className="Sidebar-Links">
+            {ContentList[section].map(link => (
+              <li key={link.Icon}>
+                <a
+                  href={link.Link}
+                  className={link.Icon === "news" ? "active" : null}
+                >
+                  <span className={"Sprite-" + link.Icon} />
+                  {link.Text}
+                </a>
+              </li>
+            ))}
+          </ul>
+        </Fragment>
+      ))}
     </div>
   );
 };
