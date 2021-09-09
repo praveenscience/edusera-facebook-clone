2fa163cf56ce3efcedea36951399d85948a71278
Praveen Kumar <praveenscience@gmail.com>
2021-09-09 07:07:42 +0100

    Fixed the colours & headers.

--- src/components/Bootstrap/Header.js
+++ src/components/Bootstrap/Header.js
@@ -13,6 +13,16 @@ const Header = ({ dark, children, className }) => {
           <img src="/logo512.png" alt={children + " Logo"} className="me-2" />
           {children}
         </a>
+        <div className="UserMeta">
+          <a href="/">
+            <img src="https://i.imgur.com/QDBSMr2b.jpg" alt="Marku" />
+            Zuck
+          </a>
+          <span className="nav-links">
+            <a href="/">Home</a>
+            <a href="/">Create</a>
+          </span>
+        </div>
       </div>
     </nav>
   );
