247acbfde8d303898da01eb7f96189404b40fa8c
Praveen Kumar <praveenscience@gmail.com>
2021-09-09 07:14:17 +0100

    Created a sidebar component with UserMeta.

---
+++ src/components/Sidebar.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Sidebar = () => {
+  return (
+    <div className="Sidebar col-2">
+      <div className="UserMeta">
+        <a href="/">
+          <img src="https://i.imgur.com/QDBSMr2b.jpg" alt="Marku" />
+          Zuck
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
