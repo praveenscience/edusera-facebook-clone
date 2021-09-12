f4972b4cbb717cf7560a1f89f06ff52def87356c
Praveen Kumar <praveenscience@gmail.com>
2021-09-12 06:50:21 +0100

    Added the basic feed skeleton.

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import { Component } from "react";
 import Header from "./Bootstrap/Header";
+import Feed from "./Feed";
 import Sidebar from "./Sidebar";
 
 class App extends Component {
@@ -11,6 +12,7 @@ class App extends Component {
           <div className="row">
             <Sidebar />
             <div className="col-7">Middle</div>
+            <Feed />
           </div>
         </div>
       </div>
---
+++ src/components/Feed.js
@@ -0,0 +1,13 @@
+import FBBox from "./FBBox";
+
+const Feed = () => {
+  return (
+    <div className="col-3">
+      <FBBox>Birthdays</FBBox>
+      <FBBox>Pages</FBBox>
+      <FBBox>Feed</FBBox>
+    </div>
+  );
+};
+
+export default Feed;
