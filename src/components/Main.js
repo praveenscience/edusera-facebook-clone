7143a5ed1785f88539514e5e27cdef6733c3cdd5
Praveen Kumar <praveenscience@gmail.com>
2021-09-12 06:57:01 +0100

    Created a Main section with empty skeleton for Create Post.

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,7 +1,12 @@
 import React from "react";
+import FBBox from "./FBBox";
 
 const Main = () => {
-  return <div className="col-7">Middle</div>;
+  return (
+    <div className="col-7">
+      <FBBox Heading="Create a Post"></FBBox>
+    </div>
+  );
 };
 
 export default Main;
