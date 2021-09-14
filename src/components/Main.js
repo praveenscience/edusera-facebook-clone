fccb17322c3310fd5a28a4fc64cb5bb9b6e27043
Praveen Kumar <praveenscience@gmail.com>
2021-09-14 07:17:22 +0100

    Different types of Attachments with Text on Main.

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,13 +1,38 @@
 import React from "react";
 import FBBox from "./FBBox";
+import PostUserMeta from "./PostUserMeta";
 
 const Main = ({ WallData }) => {
   return (
-    <div className="col-7">
+    <div className="col-7 Main">
       <FBBox Heading="Create a Post"></FBBox>
-      <pre className="bg-light border rounded p-2">
-        {JSON.stringify(WallData, null, 2)}
-      </pre>
+      {WallData.map((item, key) => (
+        <FBBox key={key}>
+          <PostUserMeta {...item.UserMeta} />
+          <p>{item.Text}</p>
+          {item.Attachment && (
+            <>
+              {item.Type === "Picture" && (
+                <img src={item.Attachment} alt={item.Text} />
+              )}
+              {item.Type === "Video" && (
+                <div class="embed-container">
+                  <iframe
+                    src={
+                      "https://www.youtube-nocookie.com/embed/" +
+                      item.Attachment.replace("https://youtu.be/", "") +
+                      "?controls=0&autohide=2"
+                    }
+                    title={item.Text}
+                    frameborder="0"
+                    allowfullscreen
+                  ></iframe>
+                </div>
+              )}
+            </>
+          )}
+        </FBBox>
+      ))}
     </div>
   );
 };
