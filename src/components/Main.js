6d5ad0b33221a4f24e379f8e044f2c3a6a370532
Praveen Kumar <praveenscience@gmail.com>
2021-09-14 07:19:04 +0100

    Created PostAttachment Component.

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,5 +1,6 @@
 import React from "react";
 import FBBox from "./FBBox";
+import PostAttachment from "./PostAttachment";
 import PostUserMeta from "./PostUserMeta";
 
 const Main = ({ WallData }) => {
@@ -10,27 +11,7 @@ const Main = ({ WallData }) => {
         <FBBox key={key}>
           <PostUserMeta {...item.UserMeta} />
           <p>{item.Text}</p>
-          {item.Attachment && (
-            <>
-              {item.Type === "Picture" && (
-                <img src={item.Attachment} alt={item.Text} />
-              )}
-              {item.Type === "Video" && (
-                <div class="embed-container">
-                  <iframe
-                    src={
-                      "https://www.youtube-nocookie.com/embed/" +
-                      item.Attachment.replace("https://youtu.be/", "") +
-                      "?controls=0&autohide=2"
-                    }
-                    title={item.Text}
-                    frameborder="0"
-                    allowfullscreen
-                  ></iframe>
-                </div>
-              )}
-            </>
-          )}
+          <PostAttachment item={item} />
         </FBBox>
       ))}
     </div>
