e136e99a15d5c239188ccbb50ad47b2448132d68
Praveen Kumar <praveenscience@gmail.com>
2021-09-14 07:07:59 +0100

    Created a Post User Meta.

---
+++ src/components/PostUserMeta.js
@@ -0,0 +1,23 @@
+import TimeAgo from "react-timeago";
+
+const PostUserMeta = ({ FullName, Avatar, ProfileURL, Location, DateTime }) => {
+  return (
+    <div className="PostUserMeta">
+      <a href={ProfileURL}>
+        <img src={Avatar} alt={FullName} />
+      </a>
+      <div className="PostUserMeta-Meta">
+        <p className="m-0">
+          <a href={ProfileURL}>
+            <strong>{FullName}</strong>
+          </a>
+        </p>
+        <p className="m-0 text-muted">
+          <TimeAgo date={DateTime} /> &bull; {Location}
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default PostUserMeta;
